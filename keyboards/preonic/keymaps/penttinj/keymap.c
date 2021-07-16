3fb80555f0a61f08e7844684f030c3afabae67a4
Johan Penttinen <johanlepenttinen@gmail.com>
2021-07-16 17:10:36 +0300

    checkout

--- keyboards/preonic/keymaps/penttinj/keymap.c
+++ keyboards/preonic/keymaps/penttinj/keymap.c
@@ -31,7 +31,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[0] = LAYOUT_preonic_2x2u(
 	KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
 	KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_RBRC,
-	KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_NUHS,
+	KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_MINS, KC_NUHS,
 	KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
 	KC_LCTL, KC_LGUI, KC_LALT, MO(2),      KC_SPC    ,       KC_ENT    ,    MO(3),   KC_RALT, KC_MINS, KC_EQL
 	),
@@ -118,7 +118,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		)
 };
 
-
+#ifdef AUDIO_ENABLE
+    float foo_song[][2] = SONG(NOCTURNE_OP_9_NO_1);
+#endif
 
 /* Use this function to do stuff based on what key was pressed */
 /* bool process_record_user(uint16_t keycode, keyrecord_t *record) {
@@ -131,9 +133,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     return true;
 }; */
 
-#ifdef AUDIO_ENABLE
-    float foo_song[][2] = SONG(PREONIC_SOUND);
-#endif
 
 // Gets called on powerup atleast
 void dip_update(uint8_t index, bool active) {
