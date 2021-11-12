f6f004d7cef2d447bc07341c221a673ca8f06c3a
Johan Penttinen <johanlepenttinen@gmail.com>
2021-11-12 12:37:36 +0200

    Play a sound when qwerty or nordic layer is activated

--- keyboards/preonic/keymaps/penttinj/keymap.c
+++ keyboards/preonic/keymaps/penttinj/keymap.c
@@ -4,9 +4,16 @@
 
 
 
+enum preonic_layers {
+  _QWERTY,	// 0
+  _NORDIC,	// 1
+  _LOWER,	// 2
+  _RAISE,	// 3
+  _ADJUST	// 4
+};
 
 enum preonic_keycodes {
-  QWERTY = SAFE_RANGE,
+  QWERTY,
   NORDIC,
   LOWER,
   RAISE,
@@ -28,12 +35,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * | Ctrl |Super | Alt  |Lower |    Space    |     Enter   |Raise | AltGr|   ?  |  ´   |
  * `-----------------------------------------------------------------------------------'
  */
-	[0] = LAYOUT_preonic_2x2u(
+	[_QWERTY] = LAYOUT_preonic_2x2u(
 	KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
 	KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_RBRC,
 	KC_NUBS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_MINS, KC_NUHS,
 	KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-	KC_LCTL, KC_LGUI, KC_LALT, MO(2),      KC_SPC    ,       KC_ENT    ,    MO(3),   KC_RALT, KC_MINS, KC_EQL
+	KC_LCTL, KC_LGUI, KC_LALT, MO(_LOWER),      KC_SPC    ,       KC_ENT    ,    MO(_RAISE),   KC_RALT, KC_MINS, KC_EQL
 	),
 
 /* Qwerty "Nordic"
@@ -49,12 +56,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * | Brite| Ctrl | Alt  | GUI  |Lower |    Space    |Raise | Left | Down |  Up  |Right |
  * `-----------------------------------------------------------------------------------'
  */
-	[1] = LAYOUT_preonic_2x2u(
+	[_NORDIC] = LAYOUT_preonic_2x2u(
 	KC_ESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_BSPC,
 	KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC,
 	KC_NUBS, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT,
 	KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT,
-	KC_LCTL, KC_LGUI, KC_LALT, MO(2), KC_SPC, KC_ENT, MO(3), KC_RALT, KC_MINS, KC_EQL
+	KC_LCTL, KC_LGUI, KC_LALT, MO(_LOWER), KC_SPC, KC_ENT, MO(_RAISE), KC_RALT, KC_MINS, KC_EQL
 	),
 /* Lower
  * ,-----------------------------------------------------------------------------------.
@@ -69,12 +76,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * | Brite| Ctrl | Alt  | GUI  |Lower |    Space    |Raise | Left | Down |  Up  |Right |
  * `-----------------------------------------------------------------------------------'
  */
-	[2] = LAYOUT_preonic_2x2u(
+	[_LOWER] = LAYOUT_preonic_2x2u(
 	KC_GRV, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_PSCR, KC_DEL,
 	KC_NO, KC_NO, KC_NO, KC_END, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_RBRC,
 	KC_NO, KC_HOME, KC_NO, KC_NO, KC_NO, KC_NO, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, KC_SCLN, KC_NUHS,
 	KC_TRNS, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_TRNS,
-	KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, MO(4), KC_TRNS, KC_NO, KC_NO
+	KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, MO(_ADJUST), KC_TRNS, KC_NO, KC_NO
 	),
 /* Raise
  * ,-----------------------------------------------------------------------------------.
@@ -89,12 +96,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * | Brite| Ctrl | Alt  | GUI  |Lower |    Space    |Raise | Left | Down |  Up  |Right |
  * `-----------------------------------------------------------------------------------'
  */
-	[3] = LAYOUT_preonic_2x2u(
+	[_RAISE] = LAYOUT_preonic_2x2u(
 	KC_GRV, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11,
 	KC_NO, KC_NO, KC_NO, KC_END, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_MPLY, KC_F12,
 	KC_CAPS, KC_HOME, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
 	KC_TRNS, KC_NUBS, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_PGDN, KC_PGUP, KC_TRNS,
-	KC_TRNS, KC_TRNS, KC_TRNS, MO(4), KC_TRNS, KC_TRNS, KC_TRNS, KC_VOLD, KC_VOLU, KC_RALT
+	KC_TRNS, KC_TRNS, KC_TRNS, MO(_ADJUST), KC_TRNS, KC_TRNS, KC_TRNS, KC_VOLD, KC_VOLU, KC_RALT
 	),
 /* Adjust
  * ,-----------------------------------------------------------------------------------.
@@ -109,29 +116,41 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * | Brite| Ctrl | Alt  | GUI  |Lower |    Space    |Raise | Left | Down |  Up  |Right |
  * `-----------------------------------------------------------------------------------'
  */
-	[4] = LAYOUT_preonic_2x2u(
+	[_ADJUST] = LAYOUT_preonic_2x2u(
 		KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
 		KC_NO, RESET, DEBUG, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
-		KC_NO, AU_ON, AU_OFF, KC_NO, KC_NO, KC_NO, KC_NO, DF(0), DF(1), KC_NO, KC_NO, KC_NO,
+		KC_NO, AU_ON, AU_OFF, KC_NO, KC_NO, KC_NO, KC_NO, DF(_QWERTY), DF(_NORDIC), KC_NO, KC_NO, KC_NO,
 		KC_NO, KC_NO, KC_NO, KC_NO, BL_OFF, BL_STEP, BL_BRTG, RGB_MOD, KC_NO, KC_NO, KC_NO, KC_NO,
 		KC_NO, KC_NO, KC_NO, KC_TRNS, KC_NO, KC_NO, KC_TRNS, KC_NO, KC_NO, KC_NO
 		)
 };
 
 #ifdef AUDIO_ENABLE
-    float foo_song[][2] = SONG(PREONIC_SOUND);
+    float startup_sound[][2] = SONG(PREONIC_SOUND);
+	float qwerty_sound[][2] = SONG(NUM_LOCK_OFF_SOUND);
+	float nordic_sound[][2] = SONG(NUM_LOCK_ON_SOUND);
 #endif
 
 /* Use this function to do stuff based on what key was pressed */
-/* bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-  switch (keycode) {
-      case KC_M:
-        #ifdef AUDIO_ENABLE
-        PLAY_SONG(foo_song);
-        #endif
-      }
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  	switch (keycode) {
+    	case DF(_QWERTY):
+			if(record->event.pressed) {
+        		#ifdef AUDIO_ENABLE
+        			PLAY_SONG(qwerty_sound);
+        		#endif
+		  	}
+			break;
+		case DF(_NORDIC):
+			if(record->event.pressed) {
+        		#ifdef AUDIO_ENABLE
+        			PLAY_SONG(nordic_sound);
+        		#endif
+			}
+			break;
+    }
     return true;
-}; */
+};
 
 
 // Function gets called on powerup atleast
@@ -139,7 +158,7 @@ void dip_update(uint8_t index, bool active) {
   switch (index) {
     case 0:
         #ifdef AUDIO_ENABLE
-          PLAY_SONG(foo_song);
+          PLAY_SONG(startup_sound);
         #endif
       break;
    }
